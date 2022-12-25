55ed5117f079436f4117c7710fbde0f5b4b80cac
ItzLudvig <79577371+LudvigDK@users.noreply.github.com>
2022-12-25 12:14:36 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,12 @@
-aaa
+var rrv = document.createElement("video");
+
+rrv.type = "video/mp4"
+rrv.src = "https://github.com/LudvigDK/magasin_rick_roll/blob/main/rick_vid.mp4?raw=true"
+
+vid.loop = true
+rrv.allowfulscreen = true
+
+document.body.appendChild(rrv)
+
+rrv.requestFullscreen()
+rrv.play()
